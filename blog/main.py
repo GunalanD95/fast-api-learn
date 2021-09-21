25df4bccfbe3b82c99015d217c2d74c719ea1c8b
GunalanD95 <Guna19may2015@gmail.com>
2021-09-21 14:25:16 +0530

    updated

--- blog/main.py
+++ blog/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI , status , Response , HTTPException
 from fastapi import FastAPI, Depends
 from . import schemas , models
 from .db import engine ,SessionLocal
@@ -19,7 +19,7 @@ def get_db():
 
 
 
-@app.post('/student' , status_code=201)
+@app.post('/student' , status_code=status.HTTP_201_CREATED) # importing status and using it is a status code for our responses
 def create(request: schemas.Student , db : Session = Depends(get_db)): # get db is used to make connection with our database
     new_student = models.Student(name=request.name,body =request.body)
     db.add(new_student)
@@ -35,8 +35,12 @@ def all_students(db : Session = Depends(get_db)):
     return students
 
 # get all the students with ids
-@app.get('/student/{id}')
-def show(id:int, db : Session = Depends(get_db)):
+@app.get('/student/{id}' , status_code=200)
+def show(id:int,response: Response, db : Session = Depends(get_db)):
     # writing a query to get student record with {id} given and first is used to get the value that matches first
     student_id = db.query(models.Student).filter(models.Student.id == id).first()
+    if not student_id:
+        # response.status_code = status.HTTP_404_NOT_FOUND
+        # return {"data" : {f"student with id {id} is not found"}}
+        raise HTTPException(status_code=400,detail=f"student with id {id} is not found")
     return student_id
