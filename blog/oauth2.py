032beb286b7f09500244c8ea903c3758a2580810
Gunalan D <54971983+GunalanD95@users.noreply.github.com>
2021-10-06 17:06:56 +0530

    Update oauth2.py
    update

--- blog/oauth2.py
+++ blog/oauth2.py
@@ -13,7 +13,7 @@ SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
-def get_current_user(data: str = Depends(oauth2_scheme)):
+async def get_current_user(data: str = Depends(oauth2_scheme)):
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
